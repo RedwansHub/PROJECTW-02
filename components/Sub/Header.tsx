b6fa6ff17004b62bc454d962ba69cc27eaa8808f
Redwnshub <redwanmuhiadin@outlook.com>
2024-06-06 04:24:30 -0700

    4th

--- components/Sub/Header.tsx
+++ components/Sub/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 import { HiMenu, HiX } from 'react-icons/hi'
-
+import Logo from '../../public/images/logo.svg'
 type Props = {
   delay?: any
 }
@@ -30,7 +30,7 @@ const Header = ({ delay }: Props) => {
         transition={{ duration: 1.6, delay: delay | 5.8 }}
         className='w-full h-full flex justify-between items-center p-4 px-12 bg-white/60 rounded-lg shadow-lg'>
       <Link href='/' className='flex cursor-pointer gap-2 items-center '>
-        <Image src={'/Images/Logo.svg'} alt='image' width={35} height={35} />
+        <Image src={Logo} alt='image' width={35} height={35} />
         <div className='flex -space-y-2 flex-col'>
           <h2 className='font-semibold text-2xl '>Zenith</h2>
           <h2 className='font-semibold text-xs'>Construction</h2>
@@ -54,11 +54,11 @@ const Header = ({ delay }: Props) => {
         initial={{ height: 0 }}
         animate={{ height: 'auto' }}
         transition={{ duration: 0.5 }}
-        className='md:hidden flex flex-col items-center bg-white/60 rounded-lg shadow-lg p-4 mt-2'>
-        <Link onClick={closeMenu} href='/about' className='font-normal p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>About</Link>
-        <Link onClick={closeMenu} href='/portfolio' className='font-normal p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>Portfolio</Link>
-        <Link onClick={closeMenu} href='/services' className='font-normal p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>Services</Link>
-        <Link onClick={closeMenu} href='/contact' className='font-normal p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>Contact</Link>
+        className='md:hidden flex flex-col items-center bg-white/80 rounded-lg shadow-lg p-4 mt-2'>
+        <Link onClick={closeMenu} href='/about' className='font-medium p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>About</Link>
+        <Link onClick={closeMenu} href='/portfolio' className='font-medium p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>Portfolio</Link>
+        <Link onClick={closeMenu} href='/services' className='font-medium p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>Services</Link>
+        <Link onClick={closeMenu} href='/contact' className='font-medium p-2 text-sm hover:scale-105 duration-200 hover:font-bold cursor-pointer'>Contact</Link>
       </motion.div>
     )}
   </div>
